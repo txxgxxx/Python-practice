f7cc7cfd05b8dc2cf2ec88c4ec0453f3d940cf1b
txxgxxx <68065662+txxgxxx@users.noreply.github.com>
2021-01-18 10:52:32 +0900

    Update class.py

--- class.py
+++ class.py
@@ -1,33 +1,4 @@
-#인스턴스의 나이를 높여봅시다.
-class Person:
-    name = ""
-    age = 0
-
-    def said(self, sentence):
-        print(self.name, sentence)
-        print(sentence, self.name)
-
-    def getting_older(self):
-        self.age += 1
-        print("%s는 올해 %d 살이 되었다."%(self.name, self.age))
-        return self.age #return 결과값
-
-zuckerberg = Person()
-zuckerberg.name = "마크 저커버그"
-zuckerberg.age = 37
-print(zuckerberg.name, zuckerberg.age)
-
-zuckerberg.getting_older()
-
-print(zuckerberg.name, zuckerberg.age)
-
-
-elon_musk = Person()
-elon_musk.name = "엘론 머스크"
-elon_musk.age = 49
-print(elon_musk.name, elon_musk.age)
-
-elon_musk.getting_older()
-
-print(elon_musk.name, elon_musk.age)
-
+ #상속을 알아봅시다.
+ #class 자식 클래스명(부모 클래스):
+#클래스 옆에 부모 클래스를 적으면 부모 클래스의 상태와 행위를 상속받는다.
+#자식 클래스에 함수를 사용하여 self 매개변수를 활용할 수 있다.
